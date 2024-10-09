d60af65cfa517f14c583c16c2e930c103a951ac3
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-10-09 10:28:51 -0500

    partial

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,6 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+//let color1 = getInput()
+//let color2 = getInput()
+//return (color1 === undefined && color2 === undefined)
--- color-deconstructor.js
+++ color-deconstructor.js
@@ -1,8 +1,20 @@
+const getInput = require("./get-input");
+
 /******************
  * YOUR CODE HERE *
  ******************/
-
-
+function colorDeconstructor(color){
+if (color === "purple"){
+  return "red and blue"
+}else if (color === "orange"){
+  return "red and yellow"
+  } else if (color === "green"){
+    return "blue and yellow"
+  } else{
+    return "error"
+  }
+}
+  
 
 // Our setup code here. Don't touch!
 if(typeof colorDeconstructor === 'undefined') {
