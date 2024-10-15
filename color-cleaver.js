80df73037fbb36aa6327575f64334220aa8571e5
Tristan Wakefield <tristan.wakefield@yahoo.com>
2024-10-15 08:10:30 -0500

    complete

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,6 +5,9 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
-//let color1 = getInput()
-//let color2 = getInput()
-//return (color1 === undefined && color2 === undefined)
+let color1 = getInput()
+let color2 = getInput()
+if (color1 === undefined && color2 === undefined)
+    return "Please provide valid colors."
+  else if (isValidPrimary(color1) && isValidSecondary(color2))
+    return 'This is a valid color'
